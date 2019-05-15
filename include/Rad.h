26f4ba03d88b9a794e09b39aab87aa8ef474afac
Mahesh Attarde <coder.mahesh@gmail.com>
2019-05-16 00:09:05 +0530

    add istring ops

--- include/Rad.h
+++ include/Rad.h
@@ -1,4 +1,4 @@
 #ifndef _RAD_H_
 #define _RAD_H_
-
+#include "RadString.h"
 #endif
--- include/RadString.h
+++ include/RadString.h
@@ -1,18 +1,35 @@
 #ifndef _RAD_STRING_H_
 #define _RAD_STRING_H_
-
+#include <string>
 namespace radical{
 
 /* Case insensitive string Operations
  */
-class istring  : std::string{
+class istring  : public std::string{
 	public:
+		istring() : std::string(){}
+		~istring(){
+			using namespace std;
+			this->string::~string();
+		}
+		istring(const char * inCStyle) : std::string(inCStyle) {}
+		istring(std::string& inString) :  std::string(inString){}
+  
 		istring
-		operator=(const istring& inString);
+		operator=(const istring& inString){
+			std::copy(inString.begin(),inString.end(),this->begin());
+		}
 		istring
-		operator=(const char*    inCStyle);
+		operator=(const char*    inCStyle){
+			this->insert(0,inCStyle);
+		}
+
 		istring
-		operator=(const char     inChar);
+		operator=(const char     inChar){
+			this->insert(0,1,inChar);
+			this->insert(1,1,'\0');
+		}
+/*
 		istring
 		operator=(istring&& 	 inString);
 		const_reference 
@@ -27,10 +44,11 @@ class istring  : std::string{
 		operator+=(const char     inChar);
 		istring
 		operator+=(istring&& 	 inString);
+*/
 
 };
 
-
+/*
 inline bool
 operator==(const istring& inLHS, const istring& inRHS);
 inline bool
@@ -98,7 +116,7 @@ operator>=(const char * inLHS, const istring& inRHS);
 inline bool
 operator>(const char * inLHS, const istring& inRHS);
 
-
+*/
 
 //TODO:
 // Need StringRef naming over string_view
